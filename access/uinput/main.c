ac27383c382f48b93f4ea80480cd9fa4e5b443a7
memoryleak47 <memoryleak47@protonmail.ch>
2015-12-21 13:07:38 +0100

    ACCESS -> access/ideas; java -> access/java; added access/uinput

---
+++ access/uinput/main.c
@@ -0,0 +1,60 @@
+#include <linux/input.h>
+#include <linux/uinput.h>
+
+// for O_WRONLY
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// for snprintf
+#include <stdio.h>
+
+// for memset
+#include <memory.h>
+
+// for write
+#include <unistd.h>
+
+int main()
+{
+	int fd;
+
+	fd = open("/dev/input/uinput", O_WRONLY | O_NONBLOCK);
+	if (fd < 0)
+	{
+		printf("can't setup fd");
+		return 1;
+	}
+	/*ret = */ioctl(fd, UI_SET_EVBIT, EV_KEY);
+	/*ret = */ioctl(fd, UI_SET_EVBIT, EV_SYN);
+
+	// enabled 'd'
+	/*ret = */ioctl(fd, UI_SET_KEYBIT, KEY_D);
+
+	struct uinput_user_dev uidev;
+
+	memset(&uidev, 0, sizeof(uidev));
+
+	snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "uinput-sample");
+	uidev.id.bustype = BUS_USB;
+	uidev.id.vendor  = 0x1234;
+	uidev.id.product = 0xfedc;
+	uidev.id.version = 1;
+
+	/*ret = */write(fd, &uidev, sizeof(uidev));
+	/*ret = */ioctl(fd, UI_DEV_CREATE);
+
+
+	// press d
+		struct input_event ev;
+
+		memset(&ev, 0, sizeof(ev));
+
+		ev.type = EV_KEY;
+		ev.code = KEY_D;
+		ev.value = 1;
+
+		/*ret = */write(fd, &ev, sizeof(ev));
+	return 0;
+}
+
