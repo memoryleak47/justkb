c5ba80874e80d98ed080a5e4c56c33d349c4ff79
memoryleak47 <memoryleak47@protonmail.ch>
2015-12-23 08:06:50 +0100

    access/uinput/main.c error statements improved; bugfix: write() return-value on error corrected; sudo ./exe now resets keyboard-layout ... ?

--- access/uinput/main.c
+++ access/uinput/main.c
@@ -15,6 +15,9 @@
 // for write
 #include <unistd.h>
 
+// for strerror and errno
+#include <errno.h>
+
 int main()
 {
 	int fd;
@@ -22,24 +25,24 @@ int main()
 	fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK); // sometime it is called /dev/input/uinput
 	if (fd < 0) // open(…) fails if: http://linux.die.net/man/3/open
 	{
-		printf("can't setup fd\n");
+		printf("can't setup fd (%s)\n", strerror(errno));
 		return 1;
 	}
 	if (ioctl(fd, UI_SET_EVBIT, EV_KEY) != 0)
 	{
-		printf("ioctl(UI_SET_EVBI, EV_KEY) throws error\n");
+		printf("ioctl(UI_SET_EVBI, EV_KEY) throws error (%s)\n", strerror(errno));
 		return 1;
 	}
 	if (ioctl(fd, UI_SET_EVBIT, EV_SYN) != 0)
 	{
-		printf("iioctl(fd, UI_SET_EVBIT, EV_SYN) throws error\n");
+		printf("iioctl(fd, UI_SET_EVBIT, EV_SYN) throws error (%s)\n", strerror(errno));
 		return 1;
 	}
 
 	// enables 'd'
 	if (ioctl(fd, UI_SET_KEYBIT, KEY_D) != 0)
 	{
-		printf("ioctl(fd, UI_SET_KEYBIT, KEY_D) throws error\n");
+		printf("ioctl(fd, UI_SET_KEYBIT, KEY_D) throws error (%s)\n", strerror(errno));
 		return 1;
 	}
 	struct uinput_user_dev uidev;
@@ -52,14 +55,14 @@ int main()
 	uidev.id.product = 0xfedc;
 	uidev.id.version = 1;
 
-	if (write(fd, &uidev, sizeof(uidev)) != 0) // throws error
+	if (write(fd, &uidev, sizeof(uidev)) == -1) // throws error
 	{
-		printf("write(fd, &uidev, sizeof(uidev)) throws error\n");
+		printf("write(fd, &uidev, sizeof(uidev)) throws error (%s)\n", strerror(errno));
 		return 1;
 	}
 	if (ioctl(fd, UI_DEV_CREATE) != 0)
 	{
-		printf("ioctl(fd, UI_DEV_CREATE) throws error\n");
+		printf("ioctl(fd, UI_DEV_CREATE) throws error (%s)\n", strerror(errno));
 		return 1;
 	}
 
@@ -73,9 +76,9 @@ int main()
 		ev.code = KEY_D;
 		ev.value = 1;
 
-		if (write(fd, &ev, sizeof(ev)) != 0)
+		if (write(fd, &ev, sizeof(ev)) == -1)
 		{
-			printf("write(fd, &ev, sizeof(ev)) throws error\n");
+			printf("write(fd, &ev, sizeof(ev)) throws error (%s)\n", strerror(errno));
 			return 1;
 		}
 	return 0;
