3f063a271b3e093db673e70aa9939269eac62d38
memoryleak47 <memoryleak47@protonmail.ch>
2016-08-06 12:15:56 +0200

    +Jkb::type()

--- src/justkb.cpp
+++ src/justkb.cpp
@@ -1,6 +1,7 @@
 #include "justkb.hpp"
 
 #include <X11/extensions/XTest.h>
+#include <X11/keysym.h>
 #include <string>
 #include <iostream>
 
@@ -49,7 +50,15 @@ void Jkb::handleEvent(const XEvent &x_event)
 				std::cout << "q is pressed -> quitting" << std::endl;
 				running = false;
 			}
-			sendKey(42, true);
+			else if (keycode == 25) // w
+			{
+				KeySym x[] = {XK_W, XK_O, XK_W};
+				type(x, 3);
+			}
+			else
+			{
+				sendKey(42, true);
+			}
 			break;
 		}
 		case KeyRelease:
@@ -113,6 +122,30 @@ void Jkb::sendKeys(KeySym keysyms[], bool pressed[], unsigned int number)
 	for (unsigned int i = 0; i < number; i++)
 	{
 		XTestFakeKeyEvent(display, XKeysymToKeycode(display, keysyms[i]), pressed[i], 0);
+	}
+	grab();
+}
+
+void Jkb::type(int keycodes[], unsigned int number)
+{
+	ungrab();
+	for (unsigned int i = 0; i < number; i++)
+	{
+		XTestFakeKeyEvent(display, keycodes[i], false, 0); // fixes if it was already pressed before
+		XTestFakeKeyEvent(display, keycodes[i], true, 0);
+		XTestFakeKeyEvent(display, keycodes[i], false, 0);
+	}
+	grab();
+}
+
+void Jkb::type(KeySym keysyms[], unsigned int number)
+{
+	ungrab();
+	for (unsigned int i = 0; i < number; i++)
+	{
+		XTestFakeKeyEvent(display, XKeysymToKeycode(display, keysyms[i]), false, 0);
+		XTestFakeKeyEvent(display, XKeysymToKeycode(display, keysyms[i]), true, 0);
+		XTestFakeKeyEvent(display, XKeysymToKeycode(display, keysyms[i]), false, 0);
 	}
 	grab();
 }
--- src/justkb.hpp
+++ src/justkb.hpp
@@ -16,12 +16,18 @@ class Jkb
 		static void run();
 	private:
 		static void handleEvent(const XEvent &x_event);
+
+		// inject
 		static void sendKey(int keycode, bool pressed);
 		static void sendKeys(int keycodes[], bool pressed[], unsigned int number);
 		static void sendKey(KeySym keysym, bool pressed);
 		static void sendKeys(KeySym keysyms[], bool pressed[], unsigned int number);
+		static void type(int keycodes[], unsigned int number);
+		static void type(KeySym keysyms[], unsigned int number);
+
 		static void grab();
 		static void ungrab();
+
 		static Display* display;
 		static bool running;
 
